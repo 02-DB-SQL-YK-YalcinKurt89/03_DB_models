c5414dbaecada80ddae8be18b30bda3d4bd26f58
YalcinKurt89 <yalcin.coding@gmail.com>
2023-09-06 12:59:50 +0200

    some changes

--- 01_FirstProject/sql/05_selects_1-1.sql
+++ 01_FirstProject/sql/05_selects_1-1.sql
@@ -1,1 +1,8 @@
--- to do!
+-- SELECTS 1:1
+
+-- Einzeltabelle
+SELECT * FROM mydb.cats;
+SELECT * FROM mydb.servants;
+
+-- Kreuzprodukt (Kartesisches Produkt)
+SELECT * FROM mydb.cats JOIN mydb.servants;
